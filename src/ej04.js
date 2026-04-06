044c297c7d02a9f49e618c94c9e6f052e7aebcce
ELIAN ULMAN <49090707@ort>
2026-04-06 08:50:18 -0300

    package.json

---
+++ src/ej04.js
@@ -0,0 +1,25 @@
+async function obtenerPais(nombrePais) {
+    try {
+        const  respuesta = await fetch(`https://restcountries.com/v3.1/name/${nombrePais}`);
+        
+        if (!respuesta.ok) {
+            throw new Error("País no encontrado");
+        }
+
+        const datos = await respuesta.json();
+
+        const pais = datos[0];
+
+        console.log("--- Información del País ---");
+        console.log(`País: ${pais.name.common}`);
+        console.log(`Capital: ${pais.capital ? pais.capital[0] : "N/A"}`);
+        console.log(`Región: ${pais.region}`);
+        console.log(`Población: ${pais.population.toLocaleString()}`);
+
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+
+obtenerPais("Argentina");
