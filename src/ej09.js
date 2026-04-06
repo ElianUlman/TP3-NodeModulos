4d546c6ed7737f4a1ded1af95c5e58860eaa69b7
LUCA GAMAS DE MAIO <49008593@ort>
2026-04-06 09:38:47 -0300

    ex 9 done.

--- src/ej09.js
+++ src/ej09.js
@@ -4,17 +4,38 @@ const PORT = 3000
 const app = express()
 app.use(express.json())
 
+const passMinimunLength = 8
 
 const passwordValidation = (password) => {
+    if(password.length < passMinimunLength){return false}
 
+    let containsNumber=false
+    let containsCapitalLetter = false
+
+
+    for(let i=0; i< password.length; i++){
+
+        if(!isNaN(parseInt(password[i]))){
+            containsNumber=true;
+        }
+
+        if(password[i] != password[i].toLowerCase()){
+            containsCapitalLetter=true;
+        }
+    }
+
+    if(!containsNumber || !containsCapitalLetter){return false}
+
+
+    return true
 }
 
 app.post("/ej09", (req, res) => {
+
+
+    const {password} = req.body;
     
-
-    const {text} = req.params;
-
-    res.send(analyzeText(text))
+    res.send(passwordValidation(password))
 })
 
 app.listen(PORT, () => {
